477a96e41da2705cda1bbf26d0f51f67d64b404d
Dmitriy Gordinskiy <dmitriy.gordinskiy94@gmail.com>
2023-05-07 00:00:15 +0200

    Add tests fot MappingLocator

---
+++ tests/Fixtures/Mappers/DirectoryWithSeveralMappers/UserMapper.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gordinskiy\DoctrineFluentMappingBundle\Tests\Fixtures\Mappers\DirectoryWithSeveralMappers;
+
+use LaravelDoctrine\Fluent\EntityMapping;
+use LaravelDoctrine\Fluent\Fluent;
+
+class UserMapper extends EntityMapping
+{
+    public function mapFor(): string
+    {
+        return self::class;
+    }
+
+    public function map(Fluent $builder)
+    {
+    }
+}
